0addd4c4cbcf6053f9f44b7eff5f8b7ef6a5174d
pablolpe2 <55384045+pablolpe2@users.noreply.github.com>
2020-05-22 00:45:41 +0200

    Changes

---
+++ LaberintoVR/Assets/ArreglarAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArreglarAudio : MonoBehaviour
+{
+    private int count = 0;
+    private AudioSource audio;
+    private void Start()
+    {
+        audio = this.GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (count < 11)
+        {
+            count++;
+        }
+        if (count == 10)
+        {
+            audio.bypassEffects = false;
+            audio.bypassEffects = true;
+        }
+    }
+}
