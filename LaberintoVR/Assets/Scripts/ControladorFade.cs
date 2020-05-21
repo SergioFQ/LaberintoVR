05ce3c679805f555f1795ff283356f602625ed81
Zeus guy <Zeusguy@hotmail.es>
2020-05-21 18:30:29 +0200

    Screen now fades out and then back in when starting the game

--- LaberintoVR/Assets/Scripts/ControladorFade.cs
+++ LaberintoVR/Assets/Scripts/ControladorFade.cs
@@ -7,15 +7,25 @@ public class ControladorFade : MonoBehaviour
 {
     public Animator animator;
     private string scene;
+    private vrSelection player;
+    private GameObject pos;
     public void toScene(string scene) {
         animator.SetTrigger("fadeout");
         this.scene = scene;
+    }
+    public void tpPlayer(string scene, vrSelection player, GameObject pos) {
+        animator.SetTrigger("fadeout");
+        this.scene = scene;
+        this.player = player;
+        this.pos = pos;
     }
 
     public void onFadeComplete() {
         if (scene == "SALIR") {
             //SceneManager.LoadScene("MainMenu");
             Application.Quit();
+        } else if (scene == "ButtonBegin") {
+            player.transform.position = pos.transform.position;
         } else {
             SceneManager.LoadScene(scene);
         }
